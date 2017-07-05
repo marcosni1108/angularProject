ebbf7462160c06f160245d88c20f04ee6bb3a63f
marcosni_ <marcosbatista10@gmail.com>
2017-07-05 15:56:51 -0300

    Criacao de diretivas 2

--- js/controllers/listaTelefonicaCtrl.js
+++ js/controllers/listaTelefonicaCtrl.js
@@ -2,6 +2,9 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($s
     $scope.app = "Lista Telefonica";
     $scope.contatos = [];
     $scope.operadoras = [];
+    $scope.contato = {
+        data:new Date().getTime()
+    }
 
     var carregarContatos = function () {
 
